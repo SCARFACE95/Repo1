17bc28b31a180c69f367121e7fb54117cadb6fea
SCARFACE95 <robertbst20@gmail.com>
2024-01-29 17:02:09 +0200

    task2

--- test1.py
+++ test1.py
@@ -1,1 +1,16 @@
-print("test1")
+class Cafea:
+    def __init__(self, tip, intensitate, aroma, cantitate):
+        self.tip = tip
+        self.intensitate = intensitate
+        self.aroma = aroma
+        self.cantitate = cantitate
+
+    def fierbere(self):
+        print("Fierbe bagamias pl")
+
+
+cafea_arabica = Cafea("Arabica", "Forte", "Chill", "350g")
+
+print(cafea_arabica.tip)
+cafea_arabica.fierbere()
+
